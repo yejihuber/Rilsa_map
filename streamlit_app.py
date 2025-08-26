25131eac7f7c1966381f195710bc69b7f7e11c0e
yejihuber <145437706+yejihuber@users.noreply.github.com>
2025-08-26 10:37:46 +0200

    add read_excel

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ import numpy as np
 # title
 st.title("RILSA map")
 
-# File uploader
+# File type csv and xlsx uploader
 uploaded_file = st.file_uploader("Upload a document", type=["csv", "xlsx"])
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
+    df = pd.read_excel(uploaded_file)
     st.dataframe(df)
 
 # Display data in a table format
 st.table(df)
-st.write(df)
