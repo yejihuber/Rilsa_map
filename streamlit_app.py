b2d675c09b5c5bffd9cd6fdb3958d0dc08c071da
yejihuber <145437706+yejihuber@users.noreply.github.com>
2025-08-26 10:25:38 +0200

    add title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# title
+st.title("RILSA map")
+
+# File uploader
 uploaded_file = st.file_uploader("Upload a document", type=["csv", "xlsx"])
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
