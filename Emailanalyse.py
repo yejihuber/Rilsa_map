317b5bfb6f8d8a7e810534781dba70dd62500847
yejihuber <145437706+yejihuber@users.noreply.github.com>
2025-09-12 11:23:40 +0200

    correct code

--- Emailanalyse.py
+++ Emailanalyse.py
@@ -43,12 +43,16 @@ data = pd.read_csv(csv_data, sep=',', encoding='utf-8')
 excel_df = pd.read_excel(excel_data, sheet_name=sheet_name)
 
 # fusion des deux dataframes sur la colonne 'Display Name'
-data = pd.merge(data, excel_df, on='Display Name', how='left')
+data = pd.merge(data, excel_df[['Display Name', 'Group']], on='Display Name', how='left')
 
 # =========================
-# 메인 페이지 - 데이터 표시
+# 데이터프레임 출력
 # =========================
 st.header("2. Data Overview")
 st.subheader("Raw Data")
 st.write(data)
 st.markdown("---")
+st.subheader("Data Summary")
+st.write(data.describe(include='all'))
+st.markdown("---")
+# =========================
